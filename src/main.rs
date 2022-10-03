4edaacc075b31bbef5a7587b72c95d70740fd4d9
Guy Dunton <guydunton@economist.com>
2022-10-03 09:52:06 +0100

    Update CLI to allow silent mode

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
+use clap::{Arg, ArgAction, Command};
 use dnd_parser::dnd_roll;
-use std::env;
 
 fn main() {
-    let args = env::args().collect::<Vec<String>>();
-    let result = args
-        .get(1)
-        .and_then(|arg| dnd_roll(arg))
-        .map(|roll_result| format!("{}", roll_result))
-        .unwrap_or_else(|| "Failed to parse roll. Provide argument e.g. '2d20 + 2'".to_string());
+    let matches = Command::new("Dice Roller")
+        .about("Roll polyhedral dice from the command line")
+        .arg(
+            Arg::new("silent")
+                .help("Print only the final total")
+                .long("silent")
+                .short('s')
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("INPUT")
+                .index(1)
+                .required(true)
+                .help("The text to parse e.g. 4d8 + 2"),
+        )
+        .arg_required_else_help(true)
+        .get_matches();
 
-    println!("{}", result);
+    let input = matches.get_one::<String>("INPUT").unwrap();
+    let is_silent = matches.get_one::<bool>("silent").unwrap();
+
+    match dnd_roll(input) {
+        Some(result) => {
+            if !is_silent {
+                println!("{}", result);
+            } else {
+                println!("{}", result.total());
+            }
+        }
+        None => {
+            println!("Failed to parse roll. Provide argument e.g. '2d20 + 2'");
+        }
+    }
 }
